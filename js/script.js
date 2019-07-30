7b53794e612467ae6af1688ed509f5c40c7d6b01
Andrew Julio <andrewjulio@hotmail.com>
2019-07-30 15:17:37 +0200

    Added my code for the project

--- js/script.js
+++ js/script.js
@@ -14,8 +14,50 @@ project 1 - A Random Quote Generator
   Add the `year` property to at least one object in the array.
   Use console.log() to log your array of quotes to the console.
 ***/
-
-
+const quotes = [
+  {
+    quote: "‘My uncle, what a worthy man…’",
+    source: "Alexander Pushkin (1799-1837)",
+    citation: "Eugene Onegin",
+    year: 1833
+  },
+  {
+    quote: "‘The Poet's soul did not bear // The shameful hurts of low breed,’",
+    source: "Mikhail Lermontov (1814-1841)",
+    citation: "Death Of the Poet",
+    year: 1837
+  },
+  {
+    quote: "‘I do not want to study, I want to marry!’",
+    source: "Denis Fonvizin (1744-1792)",
+    citation: "The Minor",
+    year: 1782
+  },
+  {
+    quote: "‘Manuscripts don’t burn!’",
+    source: "Mikhail Bulgakov (1891-1940)",
+    citation: "The Master and Margarita",
+    year: 1967
+  },
+  {
+    quote: "'Whether I am a trembling creature or whether I have the right?'",
+    source: "Fyodor Dostoevsky (1821–1881)",
+    citation: "Crime and Punishment",
+    year: 1866
+  },
+  {
+    quote: "‘There is yet powder in the powder-flasks!’",
+    source: "Nikolay Gogol (1809-1852)",
+    citation: "Taras Bulba",
+    year: 1835
+  },
+  {
+    quote: "'Time for business, an hour for fun'",
+    source: "Tsar Alexis of Russia (1629—1676)",
+    citation: "",
+    year: ""
+  }
+];
 
 
 /***
@@ -23,8 +65,10 @@ project 1 - A Random Quote Generator
    - Create a variable to store a random number 
    - Cse the random number to `return` a random quote object from the `quotes` array.
 ***/
-
-
+function getRandomQuote() {
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+    return(quotes[randomNumber]);
+};
 
 
 /***
@@ -32,15 +76,31 @@ project 1 - A Random Quote Generator
    - Call the `getRandomQuote` function and assign it to a variable.
    - Create a variable for the HTML string and set it equal to an empty string.
    - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
+     the random quote variable to build your HTML string.
    - Add the quote and source section to the HTML string.
    - Use an if statement to check for the citation property before adding it to the HTML string.
    - Use an if statement to check for the year property before adding it to the HTML string.
    - Don't forget to close that final `p` tag.
    - Set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
+const quote = getRandomQuote();
+console.log(quote.citation);
 
+function printQuote(quote) {
+    let htmlString = "";
+    htmlString += "<p class='quote'>" + quote.quote + "</p><br>";
+    htmlString += "<p class='source'>" + quote.source;
 
+    if(quote.citation != "") {
+      htmlString += "<span class='citation'>" + quote.citation;
+      if (quote.year != "") {
+        htmlString += "</span><span class = 'year'>" + quote.year + "</span>";
+      };
+    };
+    htmlString += "</p>";
+    return htmlString;
+};
+console.log(printQuote(quote));
 
 
 /***
